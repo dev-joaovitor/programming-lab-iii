1a41e6bc63da6b3382e58941ea7393c7abda56a3
john <joaotorvitor1@gmail.com>
2026-02-26 11:37:11 -0300

    refactor: group routes

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ import express from "express";
 import users from "./src/routes/users.js";
 
 const app = express();
+const router = app.router;
 const DEFAULT_PORT = 3000;
 
 app.use(express.json());
 app.use(express.static("assets"));
 
-app.get("/api/v1/health", function (req, res) {
+router.get("/health", function (req, res) {
   res.send("API WORKING");
 });
 
-app.use("/api/v1/users", users);
+router.use("/users", users);
 
+// prefix
+app.use("/api/v1", router);
+
+// 404
 app.use(function (req, res) {
   res.send("NOT FOUND");
 });
