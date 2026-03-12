a00e1a86a6d42b80df8d1b9719cc226d8224a68a
john <joaotorvitor1@gmail.com>
2026-03-12 11:34:30 -0300

    feat(city): add `city` data

---
+++ src/services/city.service.js
@@ -0,0 +1,46 @@
+let AUTO_INCREMENT = 1;
+const cities = [];
+
+// immutable
+function findAll() {
+  return [...cities];
+}
+
+// immutable
+function findOneById(cityId) {
+  return findAll().find((city) => city.id === cityId);
+}
+
+function findOneByName(cityName) {
+  return findAll().find(
+    (city) => city.name.toLowerCase() === cityName.toLowerCase()
+  );
+}
+
+function createOne(city) {
+  const name = city.name
+    .toLowerCase()
+    .replace(
+      /^./,
+      city.name[0].toUpperCase()
+    );
+
+  const newCity = {
+    id: AUTO_INCREMENT,
+    name,
+  }
+  cities.push(newCity);
+
+  AUTO_INCREMENT += 1;
+
+  return {...newCity};
+}
+
+const cityService = {
+  findAll,
+  findOneById,
+  findOneByName,
+  createOne,
+}
+
+export default cityService;
